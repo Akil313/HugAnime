af0f9fe3bc827b125eabea841c46da133a34ecf7
KingDzx <zachary.orr@gmail.com>
2018-04-01 16:34:55 -0400

    Zach and Rach made the Form

--- animeMan/catalog/models.py
+++ animeMan/catalog/models.py
@@ -1,3 +1,20 @@
 from django.db import models
-
+import uuid
+from django.db.models import (
+    UUIDField,
+    CharField,
+    TextField,
+    IntegerField,
+    DecimalField,
+    ImageField
+)
 # Create your models here.
+
+class AnimeCatalog(models.Model):
+    anime_id = UUIDField(primary_key = True, default=uuid.uuid4, editable=False)
+    name = CharField(max_length=100)
+    Genre = CharField(max_length=300)
+    type_Anime = CharField(max_length = 10)
+    rating = DecimalField(max_digits = 4, decimal_places = 2)
+    members = IntegerField()
+    anime_cover = ImageField(blank = True, null = True, upload_to = "img/animeCover", verbose_name = "Profile Photo")
--- animeMan/catalog/views.py
+++ animeMan/catalog/views.py
@@ -1,6 +1,26 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.views import View
+from .forms import AnimeCatalogForm
 
 # Create your views here.
 def index(request):
     return HttpResponse("Within the Catalog")
+
+class form(View):
+    def get(self, request):
+        animeCatForm = AnimeCatalogForm()
+        return render(request, 'catalogs/add.html', {
+            'form' : animeCatForm
+        })
+        #return HttpResponse("Will display Anime records ")
+
+    def post(self, request):
+        animeCatForm = AnimeCatalogForm(request.POST, request.FILES)
+        if animeCatForm.is_valid():
+            animeCatForm.save()
+            return HttpResponseRedirect("/?openr=cat&res=true")
+        return render(request, 'catalogs/add.html', {
+            'form' : animeCatForm
+        })
+        #return HttpRespons)e("Will save a new anime to the list in the database.")

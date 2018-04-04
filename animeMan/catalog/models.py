c720e29b1294fe023f64fd84d432c6d420dd68b7
KingDzx <zachary.orr@gmail.com>
2018-04-03 22:09:15 -0400

    Fixed Model

--- animeMan/catalog/models.py
+++ animeMan/catalog/models.py
@@ -15,6 +15,7 @@ class AnimeCatalog(models.Model):
     name = CharField(max_length=300)
     genre = CharField(max_length=300)
     typeanime = CharField(max_length = 10)
-    rating = DecimalField(max_digits = 4, decimal_places = 2)
+    episodes = IntegerField(default=0)
+    rating = DecimalField(max_digits = 4, decimal_places = 2, null = True)
     members = IntegerField()
     anime_cover = ImageField(blank = True, null = True, upload_to = "img/animeCover", verbose_name = "Profile Photo")

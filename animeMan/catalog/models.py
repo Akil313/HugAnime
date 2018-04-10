f25a1d863a7c9d3974c27338af8065ae9c539db3
KingDzx <zachary.orr@gmail.com>
2018-04-09 20:17:11 -0400

    Added in documentation

--- animeMan/catalog/models.py
+++ animeMan/catalog/models.py
@@ -16,11 +16,26 @@ from django.db.models import (
 Anime Catalog model used for form layout as well as database fields
 '''
 class AnimeCatalog(models.Model):
-    anime_id = CharField(primary_key = True, max_length=100) '''anime_id holds the unique identification number for each anime'''
-    name = CharField(max_length=300, default = '') '''name holds the name of the anime'''
-    genre = CharField(max_length=300, default = '') '''genre holds the various categories for each anime'''
-    typeanime = CharField(max_length = 10, default = '') '''typeAnime holds the type of anime it is, either being TV, Movie or OVA'''
-    episodes = CharField(max_length=300, default = '') ''' episodes holds the number of episode the anime has that was last recorded'''
-    rating = CharField(max_length=300, default = '') '''rating holds the average rating of the anime''' 
-    members = CharField(max_length=300, default = '') '''members holds the number of members that are accounted for in the ratings'''
-    anime_url = CharField(max_length=300, default= None, blank = True, null = True) '''anime_url holds the url of the anime image.'''
+    '''anime_id holds the unique identification number for each anime'''
+    anime_id = CharField(primary_key = True, max_length=100) 
+
+    '''name holds the name of the anime'''
+    name = CharField(max_length=300, default = '') 
+
+    '''genre holds the various categories for each anime'''
+    genre = CharField(max_length=300, default = '') 
+
+    '''typeAnime holds the type of anime it is, either being TV, Movie or OVA'''
+    typeanime = CharField(max_length = 10, default = '') 
+
+    ''' episodes holds the number of episode the anime has that was last recorded'''
+    episodes = CharField(max_length=300, default = '') 
+
+    '''rating holds the average rating of the anime''' 
+    rating = CharField(max_length=300, default = '') 
+
+    '''members holds the number of members that are accounted for in the ratings'''
+    members = CharField(max_length=300, default = '') 
+    
+    '''anime_url holds the url of the anime image.'''
+    anime_url = CharField(max_length=300, default= None, blank = True, null = True) 
--- animeMan/catalog/views.py
+++ animeMan/catalog/views.py
@@ -2,11 +2,19 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse, HttpResponseRedirect
 from django.views import View
 from django.views.generic import View
-from .forms import AnimeCatalogForm, LoginForm ''' Imports the anime catalog form and login for to be used for the html pages'''
-from .models import AnimeCatalog ''' Imports the animeCatalog model for posting data to database'''
-from django.contrib.auth import authenticate, login, get_user_model, logout 
-import csv ''' Imports CSV to get data from CSV file'''
-from django.core.paginator import Paginator ''' import statement for the paginator module so the anime could be displayed using pages and not be cluttered'''
+from .forms import AnimeCatalogForm, LoginForm 
+''' Imports the anime catalog form and login for to be used for the html pages'''
+
+from .models import AnimeCatalog 
+''' Imports the animeCatalog model for posting data to database'''
+
+from django.contrib.auth import authenticate, login, get_user_model, logout
+
+import csv 
+''' Imports CSV to get data from CSV file'''
+
+from django.core.paginator import Paginator 
+''' import statement for the paginator module so the anime could be displayed using pages and not be cluttered'''
 
 # Create your views here.
 def home(request):
@@ -21,7 +29,9 @@ def home(request):
     fiveNames = []
     names = []
 
-    new = AnimeCatalog.objects.values()''' Pulls all the data from the MySQL database table, catalog_animecatalog, and stores it in the variable new'''
+    new = AnimeCatalog.objects.values()
+    ''' Pulls all the data from the MySQL database table, catalog_animecatalog, and stores it in the variable new'''
+
 
     ''' for each entry from the database, each record's data is stored into their own individual array'''
     for line in new: 
@@ -34,16 +44,30 @@ def home(request):
         tempMembers.append(line['members'])
         pics.append(line['anime_url'])
 
-    for x in range(0, len(tempName), 5): ''' for loop to control the grouping of the entire anime collection into 5's '''
-        for y in range (5): ''' control statement for every five animes'''
-            if not(x+y >= len(tempName)): '''if the anime referenced is a valid anime then append it to the list of five animes'''
+    for x in range(0, len(tempName), 5): 
+        ''' for loop to control the grouping of the entire anime collection into 5's '''
+
+        for y in range (5): 
+            ''' control statement for every five animes'''
+
+            if not(x+y >= len(tempName)): 
+                '''if the anime referenced is a valid anime then append it to the list of five animes'''
+
                 fiveNames.append({'name':tempName[x+y],'img':pics[x+y],'id':tempID[x+y],'genre':tempGenre[x+y],'type':tempType[x+y],'episodes':tempEpisodes[x+y],'rating':tempRating[x+y],'members':tempMembers[x+y]})
-        names.append(fiveNames) '''Append the list of five animes to the list containing the lists of 5 animes'''
+        names.append(fiveNames) 
+        '''Append the list of five animes to the list containing the lists of 5 animes'''
+
         fiveNames = []
     animelist = names
-    paginator = Paginator(animelist,20) ''' holds 20 entries from the list of 5 anime'''
-    page = request.GET.get('page') ''' gets the respective page to render the data to'''
-    posts = paginator.get_page(page) ''' gets all of the pages to be posted to the webpage'''
+    paginator = Paginator(animelist,20) 
+    ''' holds 20 entries from the list of 5 anime'''
+
+    page = request.GET.get('page') 
+    ''' gets the respective page to render the data to'''
+
+    posts = paginator.get_page(page) 
+    ''' gets all of the pages to be posted to the webpage'''
+
 
     ''' renders the data that has been compiled into post onto the home html file'''
     html = render(request, 'home/home.html', {'posts':posts}) 
@@ -63,10 +87,16 @@ class form(View):
 
     ''' Takes the information entered into the form and saves it to the database'''
     def post(self, request):
-        animeCatForm =  AnimeCatalogForm() ''' sets this variable to be the form'''
-        if request.method == 'POST': ''' If the request made is a POST request'''
+        animeCatForm =  AnimeCatalogForm() 
+        ''' sets this variable to be the form'''
+
+        if request.method == 'POST': 
+            ''' If the request made is a POST request'''
+
             animeCatForm = AnimeCatalogForm(request.POST, request.FILES)
-            if animeCatForm.is_valid(): '''if the form is valid, clean of all the data that was received from the form and save it to the database'''
+            if animeCatForm.is_valid(): 
+                '''if the form is valid, clean of all the data that was received from the form and save it to the database'''
+
                 anime = AnimeCatalog()
                 anime.name = animeCatForm.cleaned_data['name']
                 anime.genre =  animeCatForm.cleaned_data['genre']
@@ -76,12 +106,16 @@ class form(View):
                 anime.members =  animeCatForm.cleaned_data['members']
                 anime.anime_url =  animeCatForm.cleaned_data['anime_url']
                 animeCatForm.save()
-                return HttpResponseRedirect("/?openr=cat&res=true")''' Redirect page to this extension if POST was successful'''
+                return HttpResponseRedirect("/?openr=cat&res=true")
+                ''' Redirect page to this extension if POST was successful'''
+
         return render(request, 'catalogs/add.html', {
             'form' : animeCatForm
-        })''' returns the form if request method is not POST'''
+        })
+        ''' returns the form if request method is not POST'''
 
-def login_view(request): ''' renders the login.html webpage'''
+def login_view(request): 
+    ''' renders the login.html webpage'''
     #print request.user.is_authenticated()
     form = LoginForm(request.POST or None)
     if form.is_valid():
